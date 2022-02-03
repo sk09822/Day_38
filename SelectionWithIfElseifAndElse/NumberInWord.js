e6c1b77d4a9cccb7f5ad2d0bb0087f2f7ce476e0
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-02-03 16:12:18 +0530

    Added : Print the Name of the Digit

---
+++ SelectionWithIfElseifAndElse/NumberInWord.js
@@ -0,0 +1,36 @@
+let number = Math.floor(Math.random() * 10)%10;
+console.log("Random Number: " + number);
+switch (number) {
+    case 0:
+        console.log("Number :0 ,In word : Zero");
+        break;
+    case 1:
+        console.log("Number :1 In word : One");
+        break;
+    case 2:
+        console.log("Number :2 In word : Two");
+        break;
+    case 3:
+        console.log("Number :3 In word : Three");
+        break;
+    case 4:
+        console.log("Number :4 In word : Four");
+        break;
+    case 5:
+        console.log("Number :5 In word : Five");
+        break;
+    case 6:
+        console.log("Number :6 In word : Six");
+        break;
+    case 7:
+        console.log("Number :7 In word : Seven");
+        break;
+    case 8:
+        console.log("Number :8 In word : Eight");
+        break;
+        case 9:
+        console.log("Number :9 In word : nine");
+        break;
+    default:
+        console.log("Enter the number in the range 0 to 9");
+}
